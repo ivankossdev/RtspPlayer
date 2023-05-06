e6fdac73894bf873b26ba1eafe2a3a9e52136560
ivankossdev <ivan.koss@yandex.ru>
2023-05-06 20:57:15 +0300

    create project

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace Player
+{
+    public partial class VideoForm : Form
+    {
+        VideoCapture capture;
+        public VideoForm()
+        {
+            InitializeComponent();
+            Hello();
+        }
+        private void Hello()
+        {
+            try
+            {
+                capture = new VideoCapture("rtsp://Admin:admin321@192.168.1.5:554/h264");
+                capture.ImageGrabbed += CurrentDevice_ImageGrabbed;
+                capture.SetCaptureProperty(CapProp.Fps, 0);
+                capture.Start();
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void CurrentDevice_ImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat(this.Size, DepthType.Cv8U, 3);
+                capture.Retrieve(m, 0);
+                pictureBox1.Image = BitmapExtension.ToBitmap(m);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
